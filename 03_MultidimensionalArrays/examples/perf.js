08ff39d4a2b96ea8cb18603c6086da0ba8ebbb41
Planeshifter <pburckhardt@outlook.com>
2015-03-06 18:25:55 -0500

    perf check

--- 03_MultidimensionalArrays/examples/perf.js
+++ 03_MultidimensionalArrays/examples/perf.js
@@ -5,10 +5,14 @@ var fill = require('ndarray-fill');
 
 var c_fill = require('../lib/fill.js').fill;
 
-var mat = zeros([100, 100]);
+var mat = zeros([400, 400]);
 
-console.log( show(mat) );
-
+console.time("Emscripten");
 var cmat = c_fill(mat, 10);
+console.timeEnd("Emscripten");
 
-console.log( show(cmat) );
+console.time("Native");
+var fmat = fill(mat, function(i, j){
+  return 10;
+});
+console.timeEnd("Native");
--- 03_MultidimensionalArrays/src/fill.c
+++ 03_MultidimensionalArrays/src/fill.c
@@ -1,4 +1,4 @@
-void fill(float **arr, int nrow, int ncol, float val) {
+void fill(double **arr, int nrow, int ncol, double val) {
   for (int i = 0; i < nrow; i++) {
     for (int j = 0; j < ncol; j++) {
       arr[i][j] = val;
